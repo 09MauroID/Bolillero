ef9cb25d6627eef95d725a4783330c8f019009c1
09MauroID <mauro.soto.et12@gmail.com>
2023-05-24 17:18:38 -0300

    Cambios SimHilos

--- Bolillero.core/Simulacion.cs
+++ Bolillero.core/Simulacion.cs
@@ -10,4 +10,11 @@ namespace Bolillero.core
         public long simularSinHilos(Bolillerito original, List<int> Jugada, int cantidad) 
             => original.JugarNVeces(Jugada, cantidad);
     }
+        public long simularconHilos(Bolillerito original, List<int> Jugada, int cantidad, int canHilos)
+        {
+            Task<long>[] cantSimul = new Task<long>[cantHilos];
+            {
+                for (int = 0, int < J)
+            }
+        }
 }

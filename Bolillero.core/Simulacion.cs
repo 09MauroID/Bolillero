75f506b4e43b4ffcba5ed0eeb966485caf2e0100
Ismael <=>
2023-06-08 17:48:33 -0300

    Cambios

--- Bolillero.core/Simulacion.cs
+++ Bolillero.core/Simulacion.cs
@@ -1,21 +1,21 @@
-namespace Bolillero.core
+namespace Bolillero.core;
+
+public class Simulacion 
 {
-    public class Simulacion 
+    public long simularSinHilos(Bolillerito clonoriginal, List<int> Jugada, int cantidad) 
+        => clonoriginal.JugarNVeces(Jugada, cantidad);
+
+    public long simularConHilos(Bolillerito original, List<int> Jugada, int cantidad, int cantHilos)
     {
-        public long simularSinHilos(Bolillerito clonoriginal, List<int> Jugada, int cantidad) 
-            => clonoriginal.JugarNVeces(Jugada, cantidad);
-    }
-        public long simularConHilos(Bolillerito clonoriginal, List<int> Jugada, int cantidad, int cantHilos)
+        var cantProcesos = cantidad / cantHilos;
+        Task<long>[] cantSimul = new Task<long>[cantHilos];
+        
+        for (int i = 0; i < cantidad; i++)
         {
-            
-            Task<long>[] cantSimul = new Task<long>[cantHilos];
-            {
-                for (int i = 0; i < cantidad; i++)
-                {
-                    var boli = clonoriginal.Clonar();
-                    cantSimul[i] = Task<long>.Run(()=>(long)boli.clonoriginal);
-                    
-                }
-            }
+            var boli = original.Clonar();
+            cantSimul[i] = Task<long>.Run(()=>(long)boli.JugarNVeces(Jugada, cantProcesos));
         }
+        Task<long>.WaitAll(cantSimul);
+        return cantSimul.Sum(c => c.Result);
+    }
 }

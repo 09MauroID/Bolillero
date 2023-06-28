2969bfd62cd79678f15e79acd9b3464a52cdc5e1
09MauroID <mauro.soto.et12@gmail.com>
2023-06-28 15:04:25 -0300

    simular

--- Bolillero.core/Simulacion.cs
+++ Bolillero.core/Simulacion.cs
@@ -1,5 +1,21 @@
 namespace Bolillero.core;
 
-public class Simulacion
+public class Simulacion 
 {
+    public long simularSinHilos(Bolillerito clonoriginal, List<int> Jugada, int cantidad) 
+        => clonoriginal.JugarNVeces(Jugada, cantidad);
+
+    public long simularConHilos(Bolillerito original, List<int> Jugada, int cantidad, int cantHilos)
+    {
+        var cantProcesos = cantidad / cantHilos;
+        Task<long>[] cantSimul = new Task<long>[cantHilos];
+        
+        for (int i = 0; i < cantidad; i++)
+        {
+            var boli = original.Clonar();
+            cantSimul[i] = Task<long>.Run(()=>(long)boli.JugarNVeces(Jugada, cantProcesos));
+        }
+        Task<long>.WaitAll(cantSimul);
+        return cantSimul.Sum(c => c.Result);
+    }
 }

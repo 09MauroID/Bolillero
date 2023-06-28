1045205037ce32d0f4ad736bf69dbe50edca6a07
09MauroID <mauro.soto.et12@gmail.com>
2023-06-28 14:44:57 -0300

    SimularTest

--- Tests/SimulacionTest.cs
+++ Tests/SimulacionTest.cs
@@ -1,12 +1,22 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Bolillero.core;
+namespace Tests;
 
-namespace Tests
+public class SimulacionTest
 {
-    public class SimulacionTest
+    public Bolillerito prueba {get; set;}
+    public SimulacionTest () => prueba = new Bolillerito(10, new PrimeAzar());
+
+    [Fact]
+    public void simularSinHilos()
     {
-        
+        var jugarNVeces = prueba.JugarNVeces(Jugada: new List<int> {0, 1}, 1);
+        Assert.Equal(1, jugarNVeces);
+    }
+
+    [Fact]
+    public void simularConHilos()
+    {
+        var jugarNVeces = prueba.JugarNVeces(Jugada: new List<int> {0,1}, 1);
+        Assert.Equal(1, jugarNVeces);
     }
 }
